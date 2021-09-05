d9c4cde4c7df8585e4024bb7ad5d4abddf8b34ec
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-09-05 12:40:57 +0530

    Updates

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,1 +1,23 @@
-emailjs.init("user_jPvpFZ5eOyUbbOPoxAPYC");
+// emailjs.init("user_jPvpFZ5eOyUbbOPoxAPYC");
+// document.getElementById("myform").addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   let name = document.getElementById("name").value;
+//   let email = document.getElementById("email").value;
+
+//   const contactParams = {
+//     from_name: name,
+//     from_email: email,
+//     messages: "test",
+//   };
+//   emailjs
+//     .send("service_qi07cwl", "template_clfjccd", contactParams)
+//     .then((res) => {
+//       console.log(res);
+//     });
+//   console.log("hello");
+// });
+
+function onSubmit() {
+  const name = document.getElementById("email").text;
+  console.log(name);
+}
