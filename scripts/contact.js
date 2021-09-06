617dfc22dde37dbd3b18cf6cfaa01593c7d4020a
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-09-06 12:04:02 +0530

    Updates

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,28 +1,22 @@
 emailjs.init("user_jPvpFZ5eOyUbbOPoxAPYC");
-const sendMail = ()=>{
+const sendMail = () => {
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let phonenumber = document.getElementById("phone").value;
-  console.log(name)
+  console.log(name);
 
-  if(name && email && phonenumber){
-  const contactParams = {
-    from_name: name,
-    from_email: email,
-    messages: "test",
-  };
-  emailjs
-    .send("service_qi07cwl", "template_clfjccd", contactParams)
-    .then((res) => {
-      console.log(res);
-    });
-  console.log("hello");
-  }else{
-    alert("Please fill all the fields")
+  if (name && email && phonenumber) {
+    const contactParams = {
+      from_name: name,
+      from_email: email,
+      messages: "test",
+    };
+    emailjs
+      .send("service_qi07cwl", "template_clfjccd", contactParams)
+      .then((res) => {
+        console.log(res);
+      });
+  } else {
+    alert("Please fill all the fields");
   }
 };
-
-// function onSubmit() {
-//   const name = document.getElementById("email").text;
-//   console.log(name);
-// }
