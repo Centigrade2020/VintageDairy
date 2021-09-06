0069c1049b80b42339b76db3658e73ce86adfd4b
Dharundds <76607616+Dharundds@users.noreply.github.com>
2021-09-06 10:37:33 +0530

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,23 +1,28 @@
-// emailjs.init("user_jPvpFZ5eOyUbbOPoxAPYC");
-// document.getElementById("myform").addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   let name = document.getElementById("name").value;
-//   let email = document.getElementById("email").value;
+emailjs.init("user_jPvpFZ5eOyUbbOPoxAPYC");
+const sendMail = ()=>{
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phonenumber = document.getElementById("phone").value;
+  console.log(name)
 
-//   const contactParams = {
-//     from_name: name,
-//     from_email: email,
-//     messages: "test",
-//   };
-//   emailjs
-//     .send("service_qi07cwl", "template_clfjccd", contactParams)
-//     .then((res) => {
-//       console.log(res);
-//     });
-//   console.log("hello");
-// });
+  if(name && email && phonenumber){
+  const contactParams = {
+    from_name: name,
+    from_email: email,
+    messages: "test",
+  };
+  emailjs
+    .send("service_qi07cwl", "template_clfjccd", contactParams)
+    .then((res) => {
+      console.log(res);
+    });
+  console.log("hello");
+  }else{
+    alert("Please fill all the fields")
+  }
+};
 
-function onSubmit() {
-  const name = document.getElementById("email").text;
-  console.log(name);
-}
+// function onSubmit() {
+//   const name = document.getElementById("email").text;
+//   console.log(name);
+// }
