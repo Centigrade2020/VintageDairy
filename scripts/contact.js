4721084c5c64614fe7c9977b2c109f318a49577b
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-09-09 14:39:13 +0530

    Updates

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,40 +1,34 @@
 emailjs.init("user_jPvpFZ5eOyUbbOPoxAPYC");
-const sendMail = ()=>{
+const sendMail = () => {
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let phonenumber = document.getElementById("phone").value;
-  console.log(name)
-// -----------------emailing---------------------------------------
-  if(name && email && phonenumber){
-  const contactParams = {
-    from_name: name,
-    from_email: email,
-    messages: "test",
-  };
-  emailjs
-    .send("service_qi07cwl", "template_clfjccd", contactParams)
-    .then((res) => {
-      console.log(res.text);
+  console.log(name);
+  // -----------------emailing---------------------------------------
+  if (name && email && phonenumber) {
+    const contactParams = {
+      from_name: name,
+      from_email: email,
+      messages: "test",
+    };
+    emailjs
+      .send("service_qi07cwl", "template_clfjccd", contactParams)
+      .then((res) => {
+        console.log(res.text);
+      });
+
+    // -------------------------Data to spreadsheet----------------------
+
+    fetch("https://api.apispreadsheets.com/data/17851/", {
+      method: "POST",
+      body: JSON.stringify({
+        data: { Name: name, Email: email, Phone: phonenumber },
+      }),
+    }).then((res) => {
+      if (res.status === 201) {
+        console.log("entered ");
+      }
     });
-  
-// -------------------------Data to spreadsheet----------------------
-                            
-
-  fetch("https://api.apispreadsheets.com/data/17851/", {
-	  method: "POST",
-	  body: JSON.stringify({"data": {"Name":name,"Email":email,"Phone":phonenumber}}),
-  }).then(res =>{
-	    if (res.status === 201){
-		  console.log("entered ")
-	  }
-	  else{
-		  alert("Some error occured please contact centigrade.ad@gmail.com")
-	  }
-})
-// -------------------------------------------------------------------
-
-  }else{
-    alert("Please fill all the fields")
+    // -------------------------------------------------------------------
   }
 };
-
